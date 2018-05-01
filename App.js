c93aabad038570ec0670af465e0a81a6116883c0
John <johnsalcedonetwork@gmail.com>
2018-05-01 03:24:06 +0100

    Add temporary swipe next button

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Dimensions } from "react-native";
 import Horizontal from "./src/components/horizontal/horizontal";
 import Vertical from "./src/components/vertical/vertical";
 
+const { width } = Dimensions.get("window");
+const images = [
+  require("./src/assets/1.jpeg"),
+  require("./src/assets/2.jpeg"),
+  require("./src/assets/3.jpeg"),
+]
 export default class App extends Component {
+  state = {
+    activeIndex: 0,
+  }
+
+  handleSwipe = () => {
+    this.setState((prevState) => ({ activeIndex: prevState.activeIndex + 1 === images.length ? 0 : prevState.activeIndex + 1 }),
+    () => this.horizontalScrollViewRef.scrollTo({ x: width * this.state.activeIndex }))
+  }
+
   render() {
     return (
       <View>
-        <Horizontal />
+        <TouchableOpacity
+          onPress={this.handleSwipe}
+          style={{ backgroundColor: "white", position: "absolute", zIndex: 999, height: 50, width: 50, justifyContent: "center", alignItems: "center", left: width / 2.5, top: 75 }}>
+          <Text>NEXT</Text>
+        </TouchableOpacity>
+        <Horizontal
+          images={images}
+          horizontalScrollViewRef={(ref) => this.horizontalScrollViewRef = ref}
+        />
         <Vertical />
       </View>
     )
--- src/components/horizontal/horizontal.js
+++ src/components/horizontal/horizontal.js
@@ -3,20 +3,17 @@ import { View, Text, ScrollView, Image, Dimensions } from "react-native";
 
 const { width } = Dimensions.get("window");
 
-const images = [
-    require("../../assets/1.jpeg"),
-    require("../../assets/2.jpeg"),
-    require("../../assets/3.jpeg"),
-]
+
 
 export default class Horizontal extends Component {
     render() {
         return (
             <View style={{ position: "absolute" }}>
                 <ScrollView
+                    ref={this.props.horizontalScrollViewRef}
                     pagingEnabled={true}
                     horizontal={true}>
-                    {images.map((image, index) => (
+                    {this.props.images.map((image, index) => (
                         <View key={index} style={{height: 200, width}}>
                             <Image
                                 style={{ height: 200, width }}
