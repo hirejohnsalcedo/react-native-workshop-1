a8fd62594580a5ecd9c1ce2822b134a6b920c325
John <johnsalcedonetwork@gmail.com>
2018-05-01 03:12:39 +0100

    Finish up Vertical ScrollView component's UI

--- src/components/vertical/vertical.js
+++ src/components/vertical/vertical.js
@@ -6,16 +6,19 @@ export default class Vertical extends Component {
         return (
             <View>
                 <ScrollView>
-                    {Array.from({ length: 20 }).map((_, index) => (
-                        <View key={index}>
-                            <Text>
-                                create mode 100644 src/assets/1.jpeg
-                                create mode 100644 src/assets/2.jpeg
-                                create mode 100644 src/assets/3.jpeg
-                                create mode 100644 src/components/horizontal/horizontal.js
+                    <View style={{ height: 200 }} />
+                    <View style={{ backgroundColor: "rgb(255, 50, 50)" }}>
+                        {Array.from({ length: 20 }).map((_, index) => (
+                            <View key={index}>
+                                <Text>
+                                    create mode 100644 src/assets/1.jpeg
+                                    create mode 100644 src/assets/2.jpeg
+                                    create mode 100644 src/assets/3.jpeg
+                                    create mode 100644 src/components/horizontal/horizontal.js
                             </Text>
-                        </View>
-                    ))}
+                            </View>
+                        ))}
+                    </View>
                 </ScrollView>
             </View>
         )
