72dd5aba7d9223f4f1c45d1cc08ac818ea1ee7ce
John <johnsalcedonetwork@gmail.com>
2018-05-01 03:39:15 +0100

    Add basic handlePanResponderMove method

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, TouchableOpacity, Dimensions } from "react-native";
+import { View, Text, TouchableOpacity, Dimensions, PanResponder } from "react-native";
 import Horizontal from "./src/components/horizontal/horizontal";
 import Vertical from "./src/components/vertical/vertical";
 
@@ -14,6 +14,18 @@ export default class App extends Component {
     activeIndex: 0,
   }
 
+  handlePanResponderMove = (_, gestureState) => {
+    const horizontalSwipeVelocity = gestureState.vx;
+    const horizontalSwipeVelocityThreshold = 0.7;
+    const hasSwipedRight = horizontalSwipeVelocity < (horizontalSwipeVelocityThreshold * -1)
+    console.log("hasSwipedRight", hasSwipedRight)
+  }
+
+  panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: this.handlePanResponderMove,
+  })
+
   handleSwipe = () => {
     this.setState((prevState) => ({ activeIndex: prevState.activeIndex + 1 === images.length ? 0 : prevState.activeIndex + 1 }),
     () => this.horizontalScrollViewRef.scrollTo({ x: width * this.state.activeIndex }))
@@ -22,11 +34,9 @@ export default class App extends Component {
   render() {
     return (
       <View>
-        <TouchableOpacity
-          onPress={this.handleSwipe}
-          style={{ backgroundColor: "white", position: "absolute", zIndex: 999, height: 50, width: 50, justifyContent: "center", alignItems: "center", left: width / 2.5, top: 75 }}>
-          <Text>NEXT</Text>
-        </TouchableOpacity>
+        <View
+          {...this.panResponder.panHandlers}
+          style={{ position: "absolute", height: 200, width, backgroundColor: "rgba(0,255,0,0.7)", zIndex: 999 }} />
         <Horizontal
           images={images}
           horizontalScrollViewRef={(ref) => this.horizontalScrollViewRef = ref}
